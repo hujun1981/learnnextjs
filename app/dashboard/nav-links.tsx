9cf58c00e1b8fdba448a19404d775fe6c71084a0
hujun1981 <18687188898@163.com>
2025-11-29 09:31:53 +0800

    BUG

--- app/dashboard/nav-links.tsx
+++ app/dashboard/nav-links.tsx
@@ -1,5 +1,5 @@
 'use client';
- 
+
 import {
   UserGroupIcon,
   HomeIcon,
@@ -9,14 +9,24 @@ import {
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
- 
-// ...
- 
+
+// Map of links to display in the side navigation.
+// Depending on the size of the application, this would be stored in a database.
+const links = [
+  { name: 'Home', href: '/dashboard', icon: HomeIcon },
+  {
+    name: 'Invoices',
+    href: '/dashboard/invoices',
+    icon: DocumentDuplicateIcon,
+  },
+  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+];
+
 export default function NavLinks() {
   const pathname = usePathname();
- 
+
   return (
-    
+    <>
       {links.map((link) => {
         const LinkIcon = link.icon;
         return (
